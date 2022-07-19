88da588951e24b6c7b9a0e64e9434435e0c32574
Yash <bajajy82@gmail.com>
2022-07-19 19:35:59 +0530

    map.js zoom on scroll working

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,7 +3,7 @@ import "./Map.css";
 
 function Map({ position }) {
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
