fc2cd13c7faf032e15d454fce18b75e1fb57eb4b
Yash <bajajy82@gmail.com>
2022-07-22 02:01:13 +0530

    Map gets centered again on new IP

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,27 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import L from "leaflet";
 import "./Map.css";
 
+const myIcon = L.icon({
+  iconUrl: require("../../images/icon-location.png"),
+  iconSize: [32, 32],
+  iconAnchor: [32, 64],
+  popupAnchor: null,
+  shadowUrl: null,
+  shadowSize: null,
+  shadowAnchor: null,
+});
+
 function Map({ position }) {
+  function ChangeView({ center, zoom }) {
+    const map = useMap();
+    map.setView(center, zoom);
+    return null;
+  }
+
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+      <ChangeView center={position} zoom={13} />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
